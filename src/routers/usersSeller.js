e9337a7676920666d953644ab14be48369f139c9
Ramdhan Setiadhi <100844554+ramdhanstdi@users.noreply.github.com>
2022-08-04 11:59:17 +0700

    Revert "feat: CRUD-users-seller"

--- src/routers/usersSeller.js
+++
@@ -1,6 +0,0 @@
-const usersSeller = require('express').Router();
-const usersSellerController = require('../controllers/usersSeller');
-
-usersSeller.post('/'),usersSellerController.createUsersSeller;
-
-module.exports=usersSeller;
